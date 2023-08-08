f4d0d6bdd0d2f48c015c9cc46ccbba40f95bb295
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-08-08 12:02:20 +0500

    working expenses

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,35 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id : 'el1',
+      amount : '$750.00',
+      title : 'Toilet paper',
+      date : new Date(2020,7,14 ),
+    },
+    {
+      id : 'el2',
+      amount : '$850.00',
+      title : 'Car Insurance',
+      date : new Date(2020,9,14 ),
+    },
+    {
+      id : 'el3',
+      amount : '$950.00',
+      title : 'New Desk Wooden',
+      date : new Date(2020,10,14 ),
+    },
+    {
+      id : 'el4',
+      amount : '$650.00',
+      title : 'Coffee Maker',
+      date : new Date(2020,11,14 ),
+    }
+  ]
   return (
-   <ExpenseItem/>
+    <div>
+   <Expenses item = {expenses}/>
+   </div>
   );
 }
 

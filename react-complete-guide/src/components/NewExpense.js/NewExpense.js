b1fd8f185a9d049ca8601d4a8092e69f176d9614
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-08-11 08:22:30 +0500

    New Updates

--- react-complete-guide/src/components/NewExpense.js/NewExpense.js
+++ react-complete-guide/src/components/NewExpense.js/NewExpense.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './NewExpense.css'
-const NewExpense = () => {
+import React from "react";
 
-    return (
-        <div className="new-expense">
-            
-        </div>
-    )
-}
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
 export default NewExpense;
